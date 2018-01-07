d89e25ae9fd17b6ab6f99a42a5f55bf129395792
C4Q <c4q@C4Qs-MacBook-Pro-2.local>
2018-01-06 21:33:11 -0500

    loops done

--- src/Loops.java
+++ src/Loops.java
@@ -3,7 +3,39 @@
  */
 public class Loops {
 
-  public static void main (String args[]) {
+    public static void main(String args[]) {
 
-  }
+        //loopOneHundred();
+        //loopOneHundredWhile();
+        loopEven(10);
+
+
+    }
+
+    public static void loopOneHundred() {
+
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+
+        }
+    }
+
+    public static void loopOneHundredWhile(){
+        int counter = 1;
+        while (counter <= 100) {
+            System.out.println(counter);
+            counter++;
+
+        }
+
+
+    } public static void loopEven(int num){
+        for (int i = 2; i <=num ; i+=2) {
+            System.out.println(i);
+
+        }
+
+    }
+
 }
+
--- src/Methods.java
+++ src/Methods.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 /**
  * Created by alexandraqin on 4/14/15.
  */
@@ -12,6 +14,8 @@ public class Methods {
         System.out.println("This is room " + roomNum);
         System.out.println("e is close to " + valueToE);
         System.out.println("I am learning a bit about " + comp);
+
+        prettyInteger(6);
 
 
     }
@@ -29,14 +33,40 @@ public class Methods {
 
     }
 
-    public int greatestCommonFactor(int num, int num2) {
-        while (num != 0 && num2 != 0) // until either one of them is 0
+    public static int greatestCommonFactor(int num, int num2) {
+        while (num != 0 && num2 != 0)
         {
             int c = num2;
             num2 = num % num2;
             num = c;
         }
-        return num + num2; // either one is 0, so return the non-zero value
+        return num + num2;
+    }
+    public static boolean isMultiple(int num, int num2){
+        if (num%num2==0) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static void prettyInteger(int num){
+        String word = "";
+
+        for (int i = 0; i <=num ; i++) {
+            word += "*";
+
+
+        }
+
+        System.out.println(word+String.valueOf(num)+word);
+
+    } public static int randomInt(){
+        Random random = new Random();
+        int randNum = random.nextInt(100)+1;
+
+        return randNum;
+
     }
 
 
